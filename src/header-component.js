eee767f086cecc8225d59f9b3d03b5d56223ec0c
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-03-11 13:59:51 -0700

    added makeProfile function to header-component

--- src/header-component.js
+++ src/header-component.js
@@ -1,8 +1,22 @@
+import { auth } from './firebase.js';
+
 export function makeHeader() {
     const html = /*html*/
     `  <header>
     <img src="assets/alchemy-logo.png">
     <h1>Favorite Books</h1></header>`;
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
+
+export function makeProfile(user) {
+    const html = /*html*/ `<div class="profile">
+    <img src="http://www.nacdnet.org/wp-content/uploads/2016/06/person-placeholder.jpg">
+    <span>Megan Marshall</span>
+    <button>Sign Out</button>
+</div>`;
+
     const template = document.createElement('template');
     template.innerHTML = html;
     return template.content;
--- test/header-component.test.js
+++ test/header-component.test.js
@@ -1,8 +1,6 @@
-import { makeHeader } from '../src/header-component.js';
+import { makeHeader, makeProfile } from '../src/header-component.js';
 
 const test = QUnit.test;
-
-
 
 test('make header', assert => {
     const dom = makeHeader();
@@ -13,4 +11,24 @@ test('make header', assert => {
         <h1>Favorite Books</h1></header>`;
 
     assert.htmlEqual(dom, expected);
+});
+
+test('make user profile', assert => {
+    const user = {
+        displayName: 'Megan Marshall',
+        photoUrl: 'http://www.nacdnet.org/wp-content/uploads/2016/06/person-placeholder.jpg'
+    };
+
+
+    const expected = /*html*/
+    `  <div class="profile">
+    <img src="http://www.nacdnet.org/wp-content/uploads/2016/06/person-placeholder.jpg">
+    <span>Megan Marshall</span>
+    <button>Sign Out</button>
+</div>`;
+    
+    const dom = makeProfile(user);
+
+    assert.htmlEqual(dom, expected);
+
 });

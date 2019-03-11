a357ae66a5639ebdf9b72c78e0564fc115cd1665
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-03-11 16:39:26 -0700

    wrote and tested readFromQuery

--- src/query-component.js
+++ src/query-component.js
@@ -4,3 +4,24 @@ export function writeSearchToQuery(existingQuery, searchTerm) {
     searchParams.set('page', 1);
     return searchParams.toString();
 }
+
+export function writePageToQuery(existingQuery, page) {
+    const searchParams = new URLSearchParams(existingQuery);
+    searchParams.set('page', page);
+    return searchParams.toString();
+}
+
+
+export function readFromQuery(query) {
+    const searchParams = new URLSearchParams(query);
+    const pageString = searchParams.get('page');
+    let page = 1;
+    if(pageString) {
+        page = parseInt(pageString);
+    }
+    const options = {
+        searchTerm: searchParams.get('searchTerm'),
+        page: page
+    };
+    return options;
+}
--- test/query-component.test.js
+++ test/query-component.test.js
@@ -1,4 +1,4 @@
-import { writeSearchToQuery } from '../src/query-component.js';
+import { writeSearchToQuery, writePageToQuery, readFromQuery } from '../src/query-component.js';
 const test = QUnit.test;
 
 test('add search to empty query', assert => {
@@ -21,4 +21,35 @@ test('add search to existing query and update search url and resets page', asser
     const expected = 'searchTerm=Harry+Potter&page=1';
     //Assert
     assert.equal(result, expected);
+});
+
+test('add page to existing query', assert => {
+    //Arrange
+    const existingQuery = 'searchTerm=batman&page=3';
+    const page = 4;
+    //Act
+    const result = writePageToQuery(existingQuery, page);
+    const expected = 'searchTerm=batman&page=4';
+    //Assert
+    assert.equal(result, expected);
+});
+
+test('read options from query', assert => {
+    const query = 'searchTerm=harry+potter&page=5';
+    const expected = {
+        searchTerm: 'harry potter',
+        page: 5
+    };
+    const result = readFromQuery(query);
+    assert.deepEqual(result, expected);
+});
+
+test('default to page 1 when no page is specified', assert => {
+    const query = 'searchTerm=star+wars';
+    const expected = {
+        searchTerm: 'star wars',
+        page: 1
+    };
+    const result = readFromQuery(query);
+    assert.deepEqual(result, expected);
 });

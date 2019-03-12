3028d6f7b5ec6452bc53befc95ba05f0cf876ef7
Leslie Shea <leslieyshea@gmail.com>
2019-03-12 15:53:07 -0700

    moved detail component function to its own file

---
+++ src/detail-component.js
@@ -0,0 +1,21 @@
+export default function makeMovieDetail(movie) {
+    let backdropPath = null;
+    if(movie.backdrop_path) {
+        backdropPath = `https://image.tmdb.org/t/p/w300${movie.backdrop_path}`;
+    }
+    else {
+        backdropPath = '../assets/backdrop-placeholder.png';
+    }
+    const html = /*html*/
+    `<article>
+        <h2 id="detail-header">${movie.title}</h2>
+        <img src="${backdropPath}">
+        <p>
+            ${movie.overview}
+        </p>
+    </article>`;
+
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
--- test/detail-component.test.js
+++ test/detail-component.test.js
@@ -1,10 +1,24 @@
+import makeMovieDetail from '../src/detail-component.js';
+
 const test = QUnit.test;
 
 test('make movie detail template', assert => {
     const movie = {
-        
-    }
+        title: 'Out of the Past',
+        backdrop_path: '/aO6MUtT9D1G0LkYHOrxG8itPjiU.jpg',
+        overview: 'Jeff Bailey seems to be a mundane gas station owner in remote Bridgeport, CA. He is dating local girl Ann Miller and lives a quiet life. But Jeff has a secret past, and when a mysterious stranger arrives in town, Jeff is forced to return to the dark world he had tried to escape.'
+    };
 
+    const result = makeMovieDetail(movie);
+    const expected = /*html*/
+    `<article>
+    <h2 id="detail-header">Out of the Past</h2>
+    <img src="https://image.tmdb.org/t/p/w300/aO6MUtT9D1G0LkYHOrxG8itPjiU.jpg">
+    <p>
+        Jeff Bailey seems to be a mundane gas station owner in remote Bridgeport, CA. He is dating local girl Ann Miller and lives a quiet life. But Jeff has a secret past, and when a mysterious stranger arrives in town, Jeff is forced to return to the dark world he had tried to escape.
+    </p>
+    </article>
+    `;
 
-
+    assert.htmlEqual(result, expected);
 });

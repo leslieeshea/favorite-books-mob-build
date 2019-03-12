af493611013a1c5b666e20468f6c2cb5d64a5e5c
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-03-12 11:53:11 -0700

    Added firebase db setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,14 @@
 const config = {
     apiKey: "AIzaSyCG9hvPEpNECaGX7GOSvZrrYZQ2txN76Uc",
-    authDomain: "favorite-books-38a30.firebaseapp.com"
+    authDomain: "favorite-books-38a30.firebaseapp.com",
+    databaseURL: "https://favorite-books-38a30.firebaseio.com",
+    projectId: "favorite-books-38a30"
+ 
 };
 
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
+
+const db = firebase.database();
+export const favoritesByUserRef = db.ref('favorites-by-user');

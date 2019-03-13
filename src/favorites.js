35b64531744389036796e4047808e13557f6e0a1
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-03-13 10:31:06 -0700

    brought in updateMovies and converted user object array

--- src/favorites.js
+++ src/favorites.js
@@ -1,12 +1,18 @@
 import loadHeader from './header-component.js'
 import { auth, favoritesByUserRef } from './firebase.js';
+import convertObjectToArray from './convert-object-to-array.js';
+import { updateMovies } from './list-component.js';
 
 loadHeader();
 
 auth.onAuthStateChanged(user => {
+    const userId = user.uid;
     const userFavoritesRef = favoritesByUserRef.child(user.uid);
-    userFavoritesRef.on('value', snapshot => {
-        const value = snapshot.val();
-        const movies = 
-    })
-})
+
+    userFavoritesRef.on('value')
+        .then(snapshot => {
+            const data = snapshot.val();
+            const favoriteMovies = convertObjectToArray(data);
+            updateMovies(favoriteMovies); 
+        });
+});
